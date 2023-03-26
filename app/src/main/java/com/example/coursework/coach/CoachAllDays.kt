4d0a97beb1c1859be14b30ffc1a6f22003b090ce
nikita <Ve7ter@yandex.ru>
2023-03-26 22:46:38 +0300

    Check TODO in CoachAllDays.kt

---
+++ app/src/main/java/com/example/coursework/coach/CoachAllDays.kt
@@ -0,0 +1,18 @@
+package com.example.coursework.coach
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coursework.databinding.ActivityCoachAllDaysBinding
+
+class CoachAllDays : AppCompatActivity() {
+    private lateinit var binding: ActivityCoachAllDaysBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoachAllDaysBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // TODO: Check dialog with Sage(valorant) and add horizontal ListView in Gradle
+    }
+}
